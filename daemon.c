aa7884ccb961e76cf8c26e1bf4a490f4ef19d103
giaco <giaco14mw@gmail.com>
2026-03-16 23:54:20 +0100

    feat(daemon): give process name and id to packets

--- daemon.c
+++ daemon.c
@@ -1,4 +1,16 @@
+#include <uapi/linux/ptrace.h>
+#include <linux/sched.h>
+
+#define TASK_COMM_LEN 16
+
 int trace_tcp_connect(void *ctx) {
-    bpf_trace_printk("Nuova connessione TCP intercettata!\\n");
+
+    u64 id = bpf_get_current_pid_tigd();
+    u32 pid = id >> 32;
+
+    char comm[TASK_COMM_LEN];
+    bpf_get_current_comm(&comm, sizeof(comm));
+    bpf_trace_printk("New TCP connection traced: %s (PID: %d)!\\n", comm, pid);
+
     return 0;
 }
