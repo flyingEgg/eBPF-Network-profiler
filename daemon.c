652ed8e7eb6b092feb632523c2addee26e8bed68
giaco <giaco14mw@gmail.com>
2026-03-17 22:58:48 +0100

    fix: include types.h to avoid implicit declaration of types "u64" and "u32"

--- daemon.c
+++ daemon.c
@@ -1,13 +1,17 @@
-#include <uapi/linux/ptrace.h>
+#include <linux/ptrace.h>
 #include <linux/sched.h>
+#include <linux/types.h>
+
 
 #define TASK_COMM_LEN 16
 
 int trace_tcp_connect(void *ctx) {
 
-    u64 id = bpf_get_current_pid_tigd();
-    u32 pid = id >> 32;
+    // Obtain PID and TGID
+    __u64 id = bpf_get_current_pid_tigd();
+    __u32 pid = id >> 32;
 
+    // Get process name (e.g. "firefox", "bash")
     char comm[TASK_COMM_LEN];
     bpf_get_current_comm(&comm, sizeof(comm));
     bpf_trace_printk("New TCP connection traced: %s (PID: %d)!\\n", comm, pid);
