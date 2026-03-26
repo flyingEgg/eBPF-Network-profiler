82ef0794146fe7c71c9f48c380dfe364cff9d0db
flyingEgg <giaco14mw@gmail.com>
2026-03-26 15:52:36 +0100

    feat(debug): add debugpy support for debugging the sensor

--- net_sensor.py
+++ net_sensor.py
@@ -1,7 +1,32 @@
 import os
+import sys
+import debugpy
 
 from bcc import BPF
 
+# Debugpy launcher
+if "--debug" in sys.argv:
+    try:                            # If there are any errors, program will continue without debugging
+        port = 10819
+        debugpy.listen(port)
+        print(f"Debugpy listening on port {port} - press F5 to attach debugger")
+        debugpy.wait_for_client()
+        print("Debugger attached, starting sensor...")
+    except KeyboardInterrupt:
+        print("\nInterrupted by user.")
+        exit(0);
+    except RuntimeError as e:
+        print(f"Error: port {port} is already in use. Please free the port or choose a different one.")
+    except Exception as e:
+        print(f"Unexpected error while setting up debugpy: {e}")
+else:
+    print ("Proceeding without debugger. To enable debugging, run with --debug flag.")
+
+def process_event(cpu, data, size):
+    event = b["events"].event(data)
+
+    process_name = event.comm.decode('utf-8', 'replace')
+    print(f"New connection from PID: {event.pid} - Process Name: {process_name}")
 
 if os.geteuid() != 0: 
     print("This program must be run as root. Exiting.")
