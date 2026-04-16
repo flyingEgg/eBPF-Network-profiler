0a164ba53cf7312c8e0f73648c013f4628d4b9f7
flyingEgg <giaco14mw@gmail.com>
2026-04-16 19:10:19 +0200

    feat(dns_thread): implement DNSThread class for capturing DNS packets

---
+++ DNSThread.py
@@ -0,0 +1,16 @@
+import threading
+
+from scapy.all import sniff
+
+class DNSThread:
+    def __init__(self, dns_cache, on_dns_captured):
+        self.dns_cache = dns_cache
+        self.callback = on_dns_captured
+
+    def start(self):
+        thread = threading.Thread(
+            target=lambda: sniff(filter="udp src port 53", prn=self.callback, store=0),
+            daemon=True
+        )
+        thread.start()
+        
